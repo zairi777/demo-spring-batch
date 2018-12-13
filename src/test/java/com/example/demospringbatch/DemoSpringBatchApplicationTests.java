c99338d825bfed3b09f4bde9d3d4102865134c52
Mohamed ZAROUAL <mzaroual@mediametrie.fr>
2018-12-13 15:20:32 +0100

    First commit

---
+++ src/test/java/com/example/demospringbatch/DemoSpringBatchApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demospringbatch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringBatchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
